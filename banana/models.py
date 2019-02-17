9b2c53a3fa73d2b99d2f3c6adba2c993d0015ea5
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2019-02-17 22:40:06 +0100

    rm some files

--- banana/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from utils.models import CreationModificationDateMixin, UrlMixin, DateMixin
-
-class Profile (models.Model):
-    user = models.OneToOne()
